de9b726bc8d42aad406fe5eb5d2f79e971ad50a6
safcsp dev <dev@test.com>
2024-02-12 22:46:30 +0400

    Add project files.

---
+++ MakePDFCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Gehtsoft.PDFFlow.Builder;
+using Gehtsoft.PDFFlow.Models.Enumerations;
+using Gehtsoft.PDFFlow.Utils;
+
+DocumentBuilder.New().AddSection()
+    .SetSize(PaperSize.A4)
+    .SetOrientation(PageOrientation.Landscape)
+    .SetMargins(50)
+    .SetStyleFont(font: Fonts.Courier(20))
+
+    .AddParagraph("Latte").SetListNumbered()
+    .ToSection()
+    .AddParagraph("Black Coffee").AddPageNumber().ToDocument()
+    
+    
+    
+    .Build("CoffeePDF.PDF");
+   
+   
